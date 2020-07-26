596405333eab2d3e4a358268fb61ab81bf164360
dinarrene <dinardavis@gmail.com>
2020-07-26 13:13:01 -0700

    Add event listeners to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * app.js */document.addEventListener('keydown', (e) => {
+ * app.js */
+
+let game = ''; 
+
+let startButton = document.getElementById('btn__reset');
+let keyboard = document.getElementById('qwerty');
+let currentPhrase = '';
+let letter = '';
+
+startButton.addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+    
+    /****** */
+    console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+    /***** */
+
+    currentPhrase = game.activePhrase.phrase;
+});
+
+keyboard.addEventListener('click', (e) => {
+    button = e.target; 
+        if(button.classList.contains("key")) {
+        game.handleInteraction(button);
+    }
+});
+
+document.addEventListener('keydown', (e) => {
     let allKeys = document.querySelectorAll('.key');
     let button = e.key;
     console.log(button);
